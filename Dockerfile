4830041ffb954a204af19d03ba1542f5e1bafe63
Andrea Tuci <tuci.registrazioni@gmail.com>
2024-03-10 21:27:36 +0100

    feat(main): experimenting with spacy. Commented pytorch

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,16 @@
-FROM python:3.8-slim AS builder
-
-WORKDIR /app
-
-COPY ./app/requirements.txt /app/requirements.txt
-
-RUN pip install --no-cache-dir --upgrade -r requirements.txt
-
 FROM python:3.8-slim
 
 WORKDIR /app
 
-COPY --from=builder /usr/local/lib/python3.8/site-packages /usr/local/lib/python3.8/site-packages
+COPY ./app/requirements.txt /app/requirements.txt
+RUN pip install --no-cache-dir --upgrade -r /app/requirements.txt
+
+RUN pip install nltk && \
+    python -m nltk.downloader vader_lexicon
+
+RUN python -m spacy download en_core_web_sm
+RUN python -m spacy download it_core_news_sm
+
 
 COPY ./app /app
 COPY start.sh /start.sh
--- app/main.py
+++ app/main.py
@@ -2,10 +2,9 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from sentiment_analysis import analyze_sentiment
 import spacy
-from transformers import pipeline
 from utils import *
-import torch
-from transformers import DistilBertTokenizer, DistilBertForSequenceClassification
+#import torch
+#from transformers import pipeline, DistilBertTokenizer, DistilBertForSequenceClassification
 
 
 class TextToAnalyze(BaseModel):
@@ -31,15 +30,28 @@ async def analyze(text_to_analyze: TextToAnalyze):
 @app.post("/analyze-with-spacy/")
 async def analyze_with_spacy(text_to_analyze: TextToAnalyze):
     try:
-        nlp = spacy.load("en_core_web_sm")
+        #nlp = spacy.load("en_core_web_sm")
+        nlp = spacy.load("it_core_news_sm")
         doc = nlp(text_to_analyze.text)
-        print_rich_info(doc)
+        print_rich_info(doc.cats)
+
+        results = {
+            'tokens': [{'text': token.text, 'pos': token.pos_} for token in doc],
+            'entities': [{'text': ent.text, 'label': ent.label_} for ent in doc.ents],
+            'lemma': [{'text': token.text, 'lemma': token.lemma_} for token in doc],
+            'parsing': [{'text': token.text, 'dep': token.dep_} for token in doc],
+            'sentences': [{'text': sent.text} for sent in doc.sents],
+            'sentiment': doc.cats
+        }
+
+        print_rich_info(results)
+
         return {"response": "success"}
     except Exception as e:
         print_exception()
         raise HTTPException(status_code=500, detail=str(e))
 
-@app.post("/analyze-with-transformers/")
+""" @app.post("/analyze-with-transformers/")
 async def analyze_with_transformers(text_to_analyze: TextToAnalyze):
     tokenizer = DistilBertTokenizer.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
     model = DistilBertForSequenceClassification.from_pretrained("distilbert-base-uncased-finetuned-sst-2-english")
@@ -54,4 +66,4 @@ async def analyze_with_transformers(text_to_analyze: TextToAnalyze):
         return predicted_label
     except Exception as e:
         print_exception()
-        raise HTTPException(status_code=500, detail=str(e))
+        raise HTTPException(status_code=500, detail=str(e)) """
